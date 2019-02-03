16b1a5d3face50859693099e78be8204652423b9
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2019-02-03 16:19:27 -0300

    Add a configuration for specifying the persistence file

--- src/main/scala/model/MessageHandler.scala
+++ src/main/scala/model/MessageHandler.scala
@@ -1,15 +1,10 @@
 package model
 
-import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem}
-import com.typesafe.config.{Config, ConfigFactory}
+import akka.actor.{Actor, ActorLogging, ActorRef}
 
 import message.Person.Person
 
 object MessageHandler {
-
-  def config: Config = ConfigFactory.load()
-  def system: ActorSystem = ActorSystem("PersistenceSystem")
-
   case class Persist(person: Person)
   case class MessagePersisted(person: Person)
 }
@@ -21,7 +16,6 @@ class MessageHandler(writer: ActorRef) extends Actor with ActorLogging {
   override def receive: Receive = {
 
     case Persist(person) =>
-      // Playing around with encoding/decoding for now
       log.info("Received a person message: {}", person.toByteArray.map("%02X" format _).mkString)
       writer ! person.toByteArray
       sender() ! MessagePersisted(Person.parseFrom(person.withId(person.id + 1).toByteArray))
--- src/main/scala/model/WriterHandler.scala
+++ src/main/scala/model/WriterHandler.scala
@@ -4,20 +4,25 @@ import java.io.FileOutputStream
 
 import akka.actor.{Actor, ActorLogging}
 
+import com.typesafe.config.{Config, ConfigFactory}
+
+
 object WriterHandler {
-   case object Rollover
+  def config: Config = ConfigFactory.load()
+
+  case object Rollover
 }
 
 class WriterHandler extends Actor with ActorLogging {
   import WriterHandler._
 
-  var outputFile = new FileOutputStream("blob.dat")
+  var outputFile = new FileOutputStream(config.getString("persistence.path"))
 
   override def receive: PartialFunction[Any, Unit] = {
     case byteArray:Array[Byte] =>
       outputFile.write(byteArray)
 
     case Rollover =>
-      outputFile = new FileOutputStream("blob.dat")
+      outputFile = new FileOutputStream(config.getString("persistence.path"))
   }
 }
